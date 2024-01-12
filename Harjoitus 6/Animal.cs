24eaf87d1b8ca94f5e80827d98b61a229cda0c5a
ville.sorvali <ville.sorvali@VS-ICT>
2024-01-12 11:43:09 +0200

    Add project files.

---
+++ Harjoitus 6/Animal.cs
@@ -0,0 +1,25 @@
+﻿namespace Harjoitus_6
+{
+    internal class Animal
+    {
+        public string species;
+        public string size;
+        public string danger;
+        public string canSwim;
+
+        public Animal(string species, string size, string danger, string canSwim)
+        {
+            this.species = species;
+            this.size = size;
+            this.danger = danger;
+            this.canSwim = canSwim;
+        }
+        public virtual void PrintInformation()
+        {
+            Console.WriteLine("Species: " + species);
+            Console.WriteLine("Size: " + size);
+            Console.WriteLine("Danger: " + danger);
+            Console.WriteLine("Can the animal Swim: " + canSwim);
+        }
+    }
+}
---
+++ Harjoitus 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using Harjoitus_6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance 
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        { // Species, Size, Danger, CanSwim.
+            Animal animal = new Animal("Dog", "Medium", "Low-High", "Yes");
+            animal.PrintInformation();
+        }
+    }
+}
